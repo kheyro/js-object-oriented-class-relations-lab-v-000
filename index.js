c4e3e23adb2c852ed615ad87d3ecfb9f6641f8b9
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-16 22:42:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,69 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(function (trip) {
+      return trip.driverId == this.id
+    }.bind(this))
+  }
+
+  passengers() {
+    return this.trips().map(function (trip) {
+      return trip.passenger();
+    }.bind(this));
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(function (trip) {
+      return trip.passengerId == this.id
+    }.bind(this))
+  }
+
+  drivers() {
+    return this.trips().map(function (trip) {
+      return trip.driver();
+    }.bind(this));
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+
+    store.trips.push(this)
+  }
+
+  driver() {
+    return store.drivers.find(function (d) {
+      return d.id == this.driverId
+    }.bind(this))
+  }
+
+  passenger() {
+    return store.passengers.find(function (p) {
+      return p.id == this.passengerId
+    }.bind(this))
+  }
+}
